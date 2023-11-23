256f69dc4db3b9b12c30570362b7c0588b416c13
Houda Filali <hfilali428@gmail.com>
2023-11-23 14:25:24 +0000

    commit projet final

--- src/composants/miseEnpage/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-1 bg-white text-black">{children}</div>;
-};
-
-export default Layout;
